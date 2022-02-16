381c7edc4a957fda84e4d79a1fbfb420cd713a88
Isaiah <isaiah.leiva@gmail.com>
2022-02-16 09:41:14 -0700

    commit

--- app/main.js
+++ app/main.js
@@ -8,16 +8,35 @@ window["app"] = new App();
 
 
 
-palendromeWord('Kayak', 'Pickle', 'Racecar')
+// palindromeWord('kayak', 'pickle', 'racecar')
 
+let word = 'kayak'
+let word2 = 'pickle'
+let word3 = 'racecar'
 
-function isPalendrome(str) {
-  let output = ''
-  let words = str.split('')
-  palendromeWord.forEach(word {
-    if(word.length = word.) {
-    output = word
+function palindrome(word) {
+  for (let i = 0; i < word.length; i++) {
+    let forwards = word[i]
+    let backwards = word[word.length - 1 - i]
+
+    if (forwards != backwards) {
+      return false
+    }
   }
-})
-return output
+  return true
 }
+
+
+
+//NOTE what I tried is below:
+
+// function isPalendrome(str) {
+//   let output = ''
+//   let words = str.split('')
+//   palendromeWord.forEach(word {
+//     if(firstLetter.length = lastLetter.length) {
+//     output = words
+//   }
+// })
+// return output
+// }
